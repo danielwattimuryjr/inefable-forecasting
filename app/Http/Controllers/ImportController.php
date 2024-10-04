6d3817e8b2835635dcfcb1eb383d3c7925aee112
daniel032902 <daniel032902@gmail.com>
2024-10-04 07:15:43 +0000

    20241004-import done

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\ProductCategoriesImport;
+use App\Imports\ProductsImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ImportController extends Controller
 {
-    //
+    public function productsImport(Request $request) {
+        $request->validate([
+            'file' => ['required', 'file', 'mimes:pdf,xlsx' ,'extensions:pdf,xlsx']
+        ]);
+
+        Excel::import(new ProductsImport, $request->file('file'));
+
+        return back()->with('response', [
+            'success' => true,
+            'message' => "Data produk berhasil diimport",
+        ]);
+    }
+
+    public function productCategoriesImport(Request $request) {
+        $request->validate([
+            'file' => ['required', 'file', 'mimes:pdf,xlsx' ,'extensions:pdf,xlsx']
+        ]);
+
+        Excel::import(new ProductCategoriesImport, $request->file('file'));
+
+        return back()->with('response', [
+            'success' => true,
+            'message' => "Data produk berhasil diimport",
+        ]);
+    }
 }
